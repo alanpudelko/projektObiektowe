b665167ad5cf539e0fedb36d6cd2ab15f0e4f6ef
Rafał 147719 <chx101374@student.chorzow.merito.pl>
2024-04-23 19:32:57 +0200

    Pobranie  z klucza API openweathermap działa poprawnie lecz pobranie godziny z wiednia nie działa wyskakuje bład widoczny po wybraniu Państwa Austria.

--- Austria.Designer.cs
+++ Austria.Designer.cs
@@ -33,6 +33,8 @@
             pictureBox1 = new PictureBox();
             label1 = new Label();
             label2 = new Label();
+            label3 = new Label();
+            InfoAustria = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -78,12 +80,36 @@
             label2.TabIndex = 3;
             label2.Text = "Terytorium :";
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label3.Location = new Point(1521, 156);
+            label3.Name = "label3";
+            label3.Size = new Size(204, 32);
+            label3.TabIndex = 4;
+            label3.Text = "Informacje Live :";
+            // 
+            // InfoAustria
+            // 
+            InfoAustria.BackColor = Color.FromArgb(255, 224, 192);
+            InfoAustria.Enabled = false;
+            InfoAustria.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            InfoAustria.Location = new Point(1521, 228);
+            InfoAustria.Multiline = true;
+            InfoAustria.Name = "InfoAustria";
+            InfoAustria.Size = new Size(371, 450);
+            InfoAustria.TabIndex = 5;
+            InfoAustria.TextChanged += InfoAustria_TextChanged;
+            // 
             // Austria
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(255, 224, 192);
             ClientSize = new Size(1904, 1041);
+            Controls.Add(InfoAustria);
+            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
@@ -101,5 +127,7 @@
         private Label label1;
         private Label label2;
         protected TextBox OpisAustrii;
+        private Label label3;
+        protected TextBox InfoAustria;
     }
 }
--- Austria.cs
+++ Austria.cs
@@ -1,25 +1,125 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace EuroExplorer
 {
     public partial class Austria : Form
     {
+        private Label CurrentTimeLabel = new Label();
         public Austria()
         {
             InitializeComponent();
+            Task task = DisplayWeatherAsync();
+            Controls.Add(CurrentTimeLabel);
+            CurrentTimeLabel.AutoSize = true;
+            GetCurrentTimeInVienna();
+
+        }
+ 
+
+        public class WeatherData
+        {
+            public MainData? Main { get; set; }
         }
 
+        public class MainData
+        {
+            public double Temp { get; set; }
+        }
+
+        private async Task DisplayWeatherAsync()
+        {
+            string city = "Vienna";
+            string countryCode = "AT";
+            string openWeatherMapApiKey = "22feb2021db55d19eb0608f7237cb536";
+
+            try
+            {
+                var weatherData = await GetWeatherData(city, countryCode, openWeatherMapApiKey);
+
+                if (weatherData != null)
+                {
+                    InfoAustria.Text = $"Aktualna temperatura w {city}, {countryCode}: {weatherData.Main?.Temp}°C";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd pobierania danych pogodowych: {ex.Message}");
+            }
+        }
+
+
+
+        static async Task<WeatherData?> GetWeatherData(string city, string countryCode, string apiKey)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var response = await httpClient.GetAsync($"http://api.openweathermap.org/data/2.5/weather?q={city},{countryCode}&appid={apiKey}&units=metric");
+                    response.EnsureSuccessStatusCode();
+                    var data = await response.Content.ReadFromJsonAsync<WeatherData?>();
+                    return data;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Błąd pobierania danych pogodowych: {ex.Message}");
+                }
+            }
+        }
         private void OpisAustrii_TextChanged(object sender, EventArgs e)
         {
 
         }
+        private void InfoAustria_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        private async void GetCurrentTimeInVienna()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync("http://worldtimeapi.org/api/timezone/Europe/Vienna");
+                    response.EnsureSuccessStatusCode();
+                    var content = await response.Content.ReadAsStringAsync();
+                    if (!string.IsNullOrEmpty(content))
+                    {
+                        dynamic result = JsonConvert.DeserializeObject(content);
+
+                        if (result != null)
+                        {
+                            string time = result.datetime;
+                            DateTime? viennaTime = DateTime.Parse(time);
+
+                            if (CurrentTimeLabel != null && viennaTime != null)
+                            {
+                                CurrentTimeLabel.Text = $"Aktualna godzina w Wiedniu: {viennaTime?.ToString("HH:mm:ss")}";
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Brak danych dotyczących czasu w Wiedniu.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Brak danych pobranych z serwera.");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Wystąpił błąd: {ex.Message}");
+            }
+        }
+
     }
 }
