1cbc59af3476fd8e763c61307e044a00a1adc770
Rafał 147719 <chx101374@student.chorzow.merito.pl>
2024-04-27 11:21:46 +0200

    Zmienienie pobierania czasu Wiednia z strony openweathermap.

--- Austria.cs
+++ Austria.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace EuroExplorer
 {
@@ -17,9 +19,118 @@ namespace EuroExplorer
             InitializeComponent();
         }
 
+
+
+        public class WeatherData
+        {
+            public MainData? Main { get; set; }
+        }
+
+        public class MainData
+        {
+            public double Temp { get; set; }
+        }
+
+        private async Task DisplayWeatherAsync()
+        {
+            string city = "Wiedeń";
+            string countryCode = "AT";
+            string openWeatherMapApiKey = "22feb2021db55d19eb0608f7237cb536";
+
+            try
+            {
+                var weatherData = await GetWeatherData(city, countryCode, openWeatherMapApiKey);
+
+                if (weatherData != null)
+                {
+                    InfoAustria1.Text = $"Aktualna temperatura w {city}, {countryCode}: {weatherData.Main?.Temp}°C";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd pobierania danych pogodowych: {ex.Message}");
+            }
+        }
+
+
+
+        static async Task<WeatherData?> GetWeatherData(string city, string countryCode, string apiKey)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var response = await httpClient.GetAsync($"http://api.openweathermap.org/data/2.5/weather?q={city},{countryCode}&appid={apiKey}&units=metric");
+                    response.EnsureSuccessStatusCode();
+                    var data = await response.Content.ReadFromJsonAsync<WeatherData?>();
+                    return data;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Błąd pobierania danych pogodowych: {ex.Message}");
+                }
+            }
+        }
+
+        private async Task GetCurrentTimeInViennaAsync()
+        {
+            try
+            {
+                string apiKey = "22feb2021db55d19eb0608f7237cb536";
+                string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q=Vienna&appid={apiKey}";
+
+                using (var client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        dynamic data = JObject.Parse(json);
+
+                        // Pobierz strefę czasową Wiednia
+                        int timezoneOffset = data.timezone;
+
+                        // Konwertuj timestamp na datę i godzinę
+                        DateTime viennaTime = DateTimeOffset.FromUnixTimeSeconds((long)data.dt).AddSeconds(timezoneOffset).DateTime;
+
+                        InfoAustria.Text = $"Aktualna godzina w Wiedeń: {viennaTime.ToString("HH:mm:ss")}";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Wystąpił problem podczas pobierania danych z serwera.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Wystąpił błąd: {ex.Message}");
+            }
+        }
+
+        protected override async void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            await GetCurrentTimeInViennaAsync();
+        }
         private void OpisAustrii_TextChanged(object sender, EventArgs e)
         {
 
+        private void OpisAustrii_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void InfoAustria_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InfoAustria1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
         }
     }
 }
